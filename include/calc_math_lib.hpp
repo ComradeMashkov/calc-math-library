89253b023bce209b3e96833e6c0fc18a181cf2e8
comrademashkov <comrademashkov@gmail.com>
2026-02-06 15:25:28 +0000

    Edited math lib architecture

--- include/calc_math_lib.hpp
+++ include/calc_math_lib.hpp
@@ -1,13 +1,34 @@
 #pragma once
 
+#include <cstdint>
+
 namespace mathlib {
 
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
+constexpr double DBL_MAX = 1e308;
+constexpr std::uint8_t FACTORIAL_MAX_BASE = 170;
 
-double pow(int base, int deg);
-int fac(int base);
+enum class ErrorCode : std::uint8_t {
+    OK,
+    DIVISION_BY_ZERO,
+    FACTORIAL_OF_NEGATIVE_NUMBER,
+    TYPE_OVERFLOW
+};
+
+enum class MathOperation : std::uint8_t { NONE, ADD, SUB, MUL, DIV, POW, FAC };
+
+struct MathResult {
+    std::int64_t x = 0L;
+    std::int64_t y = 0L;
+    MathOperation op = MathOperation::NONE;
+    double res = 0.0;
+};
+
+static ErrorCode check_result(double r);
+
+ErrorCode add(MathResult &result);
+ErrorCode sub(MathResult &result);
+ErrorCode mul(MathResult &result);
+ErrorCode div(MathResult &result);
+ErrorCode pow(MathResult &result);
 
 } // namespace mathlib
--- src/calc_math_lib.cpp
+++ src/calc_math_lib.cpp
@@ -1,52 +1,109 @@
 #include "calc_math_lib.hpp"
 
-#include <stdexcept>
-
 namespace mathlib {
 
-int add(int a, int b) {
-    return a + b;
-}
-
-int sub(int a, int b) {
-    return a - b;
-}
-
-int mul(int a, int b) {
-    return a * b;
-}
-
-int div(int a, int b) {
-    if (b == 0) {
-        throw std::runtime_error("No division by zero");
+ErrorCode check_result(double r) {
+    if (r > DBL_MAX || r < -DBL_MAX) {
+        return ErrorCode::TYPE_OVERFLOW;
     }
-    return a / b;
+    return ErrorCode::OK;
 }
 
-double pow(int base, int deg) {
-    double res = 1.0;
+ErrorCode add(MathResult &result) {
+    result.op = MathOperation::ADD;
 
-    if (deg > 0) {
-        for (int i = 0; i < deg; ++i) {
-            res *= base;
+    result.res = static_cast<double>(result.x) + static_cast<double>(result.y);
+    return check_result(result.res);
+}
+
+ErrorCode sub(MathResult &result) {
+    result.op = MathOperation::SUB;
+
+    result.res = static_cast<double>(result.x) - static_cast<double>(result.y);
+    return check_result(result.res);
+}
+
+ErrorCode mul(MathResult &result) {
+    result.op = MathOperation::MUL;
+
+    result.res = static_cast<double>(result.x) * static_cast<double>(result.y);
+    return check_result(result.res);
+}
+
+ErrorCode div(MathResult &result) {
+    result.op = MathOperation::DIV;
+
+    if (result.y == 0L) {
+        return ErrorCode::DIVISION_BY_ZERO;
+    }
+
+    result.res = static_cast<double>(result.x) / static_cast<double>(result.y);
+    return check_result(result.res);
+}
+
+ErrorCode pow(MathResult &result) {
+    result.op = MathOperation::POW;
+
+    if (result.y == 0L) {
+        result.res = 1.0;
+        return ErrorCode::OK;
+    }
+
+    if (result.x == 0L) {
+        if (result.y < 0L) {
+            return ErrorCode::DIVISION_BY_ZERO;
         }
-    } else if (deg < 0) {
-        for (int i = 0; i > deg; --i) {
-            res /= base;
+        result.res = 0.0;
+        return ErrorCode::OK;
+    }
+
+    double r = 1.0;
+    const std::int64_t exp = (result.y < 0L) ? -result.y : result.y;
+
+    for (int64_t i = 0L; i < exp; ++i) {
+        r *= static_cast<double>(result.x);
+        if (check_result(r) == ErrorCode::TYPE_OVERFLOW) {
+            return ErrorCode::TYPE_OVERFLOW;
         }
     }
 
-    return res;
+    if (result.y < 0L) {
+        r = 1.0 / r;
+    }
+
+    result.res = r;
+
+    return ErrorCode::OK;
 }
 
-int fac(int base) {
-    if (base < 0) {
-        throw std::runtime_error("No factorial of negative number");
+ErrorCode fac(MathResult &result) {
+    result.op = MathOperation::FAC;
+
+    if (result.x < 0L) {
+        return ErrorCode::FACTORIAL_OF_NEGATIVE_NUMBER;
     }
-    if (base <= 1) {
-        return 1;
+
+    // Assume that 170! is the upper boundary for double overflow :)
+    if (result.x > static_cast<std::int64_t>(FACTORIAL_MAX_BASE)) {
+        return ErrorCode::TYPE_OVERFLOW;
     }
-    return base * fac(base - 1);
+
+    if ((result.x == 0L) || (result.x == 1L)) {
+        result.res = 1.0;
+        return ErrorCode::OK;
+    }
+
+    MathResult prev;
+    prev.x = result.x;
+
+    const ErrorCode ec = fac(prev);
+    if (ec != ErrorCode::OK) {
+        return ec;
+    }
+
+    result.res = static_cast<double>(result.x) * prev.res;
+
+    return ErrorCode::OK;
 }
 
 } // namespace mathlib
